a2cd0aaf8e2d82e39210d098b5ac13ce3f38b246
Markus <markus.jesinghaus@codersunlimited.com>
2025-05-09 12:27:54 +0200

    Edit Route 3

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
+const path = require('path');
+
 const app = express();
 
 app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+
+const PRIVATE_APP_ACCESS = process.env.HUBSPOT_ACCESS_TOKEN;
+const CUSTOM_OBJECT_TYPE = process.env.CUSTOM_OBJECT_TYPE;
 
 
 
@@ -40,7 +47,33 @@ app.get('/update-cobj', (req, res) => {
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+    const { game_name, game_category, game_type } = req.body;
+
+    const data = {
+        properties: {
+            game_name,
+            game_category,
+            game_type
+        }
+    };
+
+    const url = `https://api.hubapi.com/crm/v3/objects/${CUSTOM_OBJECT_TYPE}`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        await axios.post(url, data, { headers });
+        res.redirect('/');
+    } catch (error) {
+        console.error('Error creating record:', error.response?.data || error.message);
+        res.status(500).send('Failed to create record');
+    }
+});
+
+app.listen(3000, () => console.log('Listening on http://localhost:3000'));
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
